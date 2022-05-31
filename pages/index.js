0092d3c73c71bdfcbd154696bd652d0f950fcf23
qiannyqian <liang.huiqian@gmail.com>
2022-05-31 15:35:06 +0800

    First commit

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,24 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from 'next/head';
+import Header from '@components/Header';
+import Footer from '@components/Footer';
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>qian.world</title>
+        <link rel="icon" href="/hibisucs-favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to qian.world" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          {/* Get started by editing <code>pages/index.js</code> */}
+          This is a WIP - check back soon!
         </p>
       </main>
 
       <Footer />
     </div>
-  )
+  );
 }
